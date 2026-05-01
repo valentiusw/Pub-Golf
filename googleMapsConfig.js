d3c4c4ad469f4a1c9d1ad74e52425cbf58377e4f
Jake Burns <jake.burns3210@gmail.com>
2026-05-02 00:05:59 +1000

    feat: add reward setup and logging functions, rename tab to Rewards/Penalties

---
+++ googleMapsConfig.js
@@ -0,0 +1,8 @@
+// API key for Google Maps and Places
+// Get from: https://console.cloud.google.com/
+// 1. Create new project
+// 2. Enable "Maps JavaScript API" and "Places API"
+// 3. Create API key, restrict to your domain
+// 4. Replace the placeholder below
+
+const GOOGLE_MAPS_API_KEY = 'AIzaSyCWeBhcC9r6Ew-5f3b3j475ctAyLsSH9Uc';
